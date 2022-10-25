05c026d89b3fc3cd39f9f3de8710367ebddcc2c2
Bedra Kraja <bedra.kraja22@gmail.com>
2022-10-25 22:26:43 +0200

    ...

--- server.ts
+++ server.ts
@@ -15,70 +15,95 @@ const prisma = new PrismaClient({ log: ['warn', 'error', 'info', 'query'] })
 //reviews
 
 app.get('/reviews', async (req, res) => {
-    const allReviews = await prisma.review.findMany({
-        include: {
-            book: true,
-            user: true
-        }
-    })
-    res.send(allReviews)
+    try {
+        const allReviews = await prisma.review.findMany({
+            include: {
+                book: true,
+                user: true
+            }
+        })
+        res.send(allReviews)
+    } catch (error) {
+        // @ts-ignore
+        res.status(400).send({ error: error.message })
+    }
 })
 
 app.get('/reviews/:id', async (req, res) => {
-    const getReviewById = await prisma.review.findUnique({
-        where: { id: Number(req.params.id) },
-        include: {
-            book: true,
-            user: true
+    try {
+        const getReviewById = await prisma.review.findUnique({
+            where: { id: Number(req.params.id) },
+            include: {
+                book: true,
+                user: true
+            }
+        })
+        if (getReviewById) {
+            res.send(getReviewById)
+        } else {
+            res.status(400).send({ error: "Review not found" })
         }
-    })
-    if (getReviewById) {
-        res.send(getReviewById)
-    } else {
-        res.status(400).send({ error: "Review not found" })
+    } catch (error) {
+        // @ts-ignore
+        res.status(400).send({ error: error.message })
     }
 })
 
 app.post('/reviews', async (req, res) => {
-    await prisma.review.create({
-        data: req.body,
-        include: {
-            book: true,
-            user: true
-        }
-    })
-    const allReviews = await prisma.review.findMany({
-        include: {
-            book: true,
-            user: true
-        }
-    })
-    res.send(allReviews)
+    try {
+        await prisma.review.create({
+            data: req.body,
+            include: {
+                book: true,
+                user: true
+            }
+        })
+        const allReviews = await prisma.review.findMany({
+            include: {
+                book: true,
+                user: true
+            }
+        })
+        res.send(allReviews)
+    } catch (error) {
+        // @ts-ignore
+        res.status(400).send({ error: error.message })
+    }
 })
 
 app.patch('/reviews/:id', async (req, res) => {
-    const updatedReview = await prisma.review.update({
-        data: req.body,
-        where: { id: Number(req.params.id) },
-        include: {
-            book: true,
-            user: true
-        }
-    })
-    res.send(updatedReview)
+    try {
+        const updatedReview = await prisma.review.update({
+            data: req.body,
+            where: { id: Number(req.params.id) },
+            include: {
+                book: true,
+                user: true
+            }
+        })
+        res.send(updatedReview)
+    } catch (error) {
+        // @ts-ignore
+        res.status(400).send({ error: error.message })
+    }
 })
 
 app.delete('/reviews/:id', async (req, res) => {
-    await prisma.review.delete({
-        where: { id: Number(req.params.id) }
-    })
-    const allReviews = await prisma.review.findMany({
-        include: {
-            book: true,
-            user: true
-        }
-    })
-    res.send(allReviews)
+    try {
+        await prisma.review.delete({
+            where: { id: Number(req.params.id) }
+        })
+        const allReviews = await prisma.review.findMany({
+            include: {
+                book: true,
+                user: true
+            }
+        })
+        res.send(allReviews)
+    } catch (error) {
+        // @ts-ignore
+        res.status(400).send({ error: error.message })
+    }
 })
 
 
