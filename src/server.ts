e97911b4ed242430167d263132b016bea52521ae
Uran Hajrizaj <uranhajrizaj@outlook.com>
2022-10-25 15:02:47 +0200

    updated

--- src/server.ts
+++ src/server.ts
@@ -112,6 +112,73 @@ app.delete("/books/:id", async (req, res) => {
 })
 
 
+//reviews
+app.get('/reviews', async (req, res) => {
+    const allReviews = await prisma.review.findMany({
+        include: {
+            book: true,
+            user: true
+        }
+    })
+    res.send(allReviews)
+})
+app.get('/reviews/:id', async (req, res) => {
+    const getReviewById = await prisma.review.findUnique({
+        where: { id: Number(req.params.id) },
+        include: {
+            book: true,
+            user: true
+        }
+    })
+    if (getReviewById) {
+        res.send(getReviewById)
+    } else {
+        res.status(400).send({ error: "Review not found" })
+    }
+})
+
+app.post('/reviews', async (req, res) => {
+    await prisma.review.create({
+        data: req.body,
+        include: {
+            book: true,
+            user: true
+        }
+    })
+    const allReviews = await prisma.review.findMany({
+        include: {
+            book: true,
+            user: true
+        }
+    })
+    res.send(allReviews)
+})
+
+app.patch('/reviews/:id', async (req, res) => {
+    const updatedReview = await prisma.review.update({
+        data: req.body,
+        where: { id: Number(req.params.id) },
+        include: {
+            book: true,
+            user: true
+        }
+    })
+    res.send(updatedReview)
+})
+
+app.delete('/reviews/:id', async (req, res) => {
+    await prisma.review.delete({
+        where: { id: Number(req.params.id) }
+    })
+    const allReviews = await prisma.review.findMany({
+        include: {
+            book: true,
+            user: true
+        }
+    })
+    res.send(allReviews)
+})
+
 app.listen(port, () => {
     console.log(`Serveri is running on: http://localhost:${port}`);
 })
